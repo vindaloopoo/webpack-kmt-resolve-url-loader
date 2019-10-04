06ac0725bfb8426de7c17c28006c52eba3a81148
Aubrey Hewes <AubreyHewes@users.noreply.github.com>
2019-10-04 16:18:25 +0200

    added eslint standards  + prettier

--- index.js
+++ index.js
@@ -31,52 +31,52 @@ module.exports = function applyLoader(content, map) {
   }
   const cb = this.async();
 
-    content = content
+  content = content
 
-        // replace /download with abs /download
-        .replace(/\/download/gim, `${baseUrl}/download`)
+    // replace /download with abs /download
+    .replace(/\/download/gim, `${options.baseUrl}/download`)
 
-        // replace /<known> with abs /<known>
-        .replace(/\/.*?\/layout\/((img|css)\/)/gim, `${baseUrl}/$1`)
+    // replace /<known> with abs /<known>
+    .replace(/\/.*?\/layout\/((img|css)\/)/gim, `${options.baseUrl}/$1`)
 
-        // remove any possible cache busters
+    // remove any possible cache busters
 
-        // clean up any image.kmt123.png
-        .replace(/\.kmt[\w]+\./gim, ".")
+    // clean up any image.kmt123.png
+    .replace(/\.kmt[\w]+\./gim, ".")
 
-        // clean up any ?r=123
-        .replace(/\?r=[\d]+/gim, "");
+    // clean up any ?r=123
+    .replace(/\?r=[\d]+/gim, "");
 
-    // now try local vs base files
-    // i.e.
-    //      custom/X/view/layout/img/test.png
-    // (if not exists) would, according to the framework, be
-    //      public_html/img/test.png
-    //
-    // This mirrors the framework expectation
-    //
-    const matches = content.match(new RegExp(`${projectRoot}[^'")]+`, "mig"));
+  // now try local vs base files
+  // i.e.
+  //      custom/X/view/layout/img/test.png
+  // (if not exists) would, according to the framework, be
+  //      public_html/img/test.png
+  //
+  // This mirrors the framework expectation
+  //
+  const matches = content.match(new RegExp(`${projectRoot}[^'")]+`, "mig"));
 
-    if (matches && matches.length) {
-        Promise.map(
-            matches,
-            uri =>
-                new Promise((resolve /* , reject */) => {
-                    fs.stat(uri, (err /* , stats */) => resolve({ uri, replace: err ? replace(uri) : false }));
-                })
-        ).then(results => {
-            results.forEach(r => {
-                if (!r.replace) {
-                    return;
-                }
-                // eslint-disable-next-line
-                console.log(r.uri, " -> ", r.replace);
-                content = content.replace(r.uri, r.replace);
-            });
-            cb(null, content, map);
-        });
-        return;
-    }
+  if (matches && matches.length) {
+    Promise.map(
+      matches,
+      uri =>
+        new Promise((resolve /* , reject */) => {
+          fs.stat(uri, (err /* , stats */) => resolve({ uri, replace: err ? replace(uri) : false }));
+        })
+    ).then(results => {
+      results.forEach(r => {
+        if (!r.replace) {
+          return;
+        }
+        // eslint-disable-next-line
+        console.log(r.uri, ' -> ', r.replace);
+        content = content.replace(r.uri, r.replace);
+      });
+      cb(null, content, map);
+    });
+    return;
+  }
 
-    cb(null, content, map);
+  cb(null, content, map);
 };
